5144d8962843147a03ffe4ad77076444c841da74
ScaryEnderman <enderslainer@gmail.com>
2014-03-13 23:12:06 +0100

    Update TextView.js

--- TextView.js
+++ TextView.js
@@ -1,8 +1,8 @@
-var GUI;
-var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-var text;
+var GUI; // define our GUI
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get(); // define our Minecraft Activity
+var text; //define our text here so it is changeable later on
 
-function useItem() {
+function useItem() { // if we tap a block
 
 ctx.runOnUiThread(new java.lang.Runnable(){
   
@@ -10,18 +10,18 @@ run: function(){
   
 try {
   
-text.setText("Tapped a block!");
-text.setTextColor(android.graphics.Color.GREEN);
+text.setText("Tapped a block!"); // our TextView's text will be set to "Tapped a block!"
+text.setTextColor(android.graphics.Color.GREEN); // our TextView's color will be set to green
 
 } catch (e){
-print ("Error: "+e)
+print ("Error: "+e) //error catch
 }
  
 }});
 
 }
 
-function newLevel(){
+function newLevel(){ //once a world is loaded
   
 ctx.runOnUiThread(new java.lang.Runnable(){
   
@@ -29,38 +29,38 @@ run: function(){
   
 try{
   
-GUI = new android.widget.PopupWindow();
+GUI = new android.widget.PopupWindow(); // set our GUI to a PopupWindow
 
-var layout = new android.widget.LinearLayout(ctx);
-text = new android.widget.TextView(ctx);
-text.setText("I'm a TextView!");
+var layout = new android.widget.LinearLayout(ctx); // make a LinearLayout where we can add our TextView
+text = new android.widget.TextView(ctx); // defining our TextView
+text.setText("I'm a TextView!"); // setting it's text to "I'm a TextView!"
 
-layout.setOrientation(android.widget.LinearLayout.VERTICAL);
-layout.setGravity(android.view.Gravity.RIGHT);
-layout.addView(text);
+layout.setOrientation(android.widget.LinearLayout.VERTICAL); // set our layout's orientation to 'VERTICAL'
+layout.setGravity(android.view.Gravity.RIGHT); // we want our text to show up at the right side of the screen
+layout.addView(text); // finally we add our TextView to the main layout
 
-GUI.setContentView(layout);
-GUI.setHeight(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);
-GUI.setWidth(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);
-GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.BOTTOM | android.view.Gravity.RIGHT, 0, 0);
+GUI.setContentView(layout); // now we need to add our layout to the gui (the pop-up)
+GUI.setHeight(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT); // we are setting the heigth to 'WRAP_CONTENT'
+GUI.setWidth(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT); // we are setting the width to 'WRAP_CONTENT'
+GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.BOTTOM | android.view.Gravity.RIGHT, 0, 0); // now let's place the gui to the bottom right of the screen
 
 } catch (e){
-print ("Error: "+e)
+print ("Error: "+e) // error catch
 }
  
 }});
 }
 
 
-function leaveGame(){
+function leaveGame(){ // if we leave our world
   
 ctx.runOnUiThread(new java.lang.Runnable(){
   
 run: function(){
   
-if(GUI != null){
+if(GUI != null){ // if the GUI exists
   
-GUI.dismiss();
+GUI.dismiss(); // close it so once we get back to the main screen of MCPE it dissapears
   
 }
 }
